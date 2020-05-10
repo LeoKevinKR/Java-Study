8facfff77a42b73fa52081cfe2734ee4af804467
Kevin Kim <33838519+LeoKevinKR@users.noreply.github.com>
2020-05-11 02:10:59 +0900

    5.11

--- mloanCopy/src/main/Policy.java
+++ mloanCopy/src/main/Policy.java
@@ -1,5 +1,19 @@
 package main;
 
-public class Policy {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Policy extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		
+		out.write("<h1>Policy Agree</h1>");
+	}
 }
